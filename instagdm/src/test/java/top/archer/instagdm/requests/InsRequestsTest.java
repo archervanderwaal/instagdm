6bc1be74381e69d1592041ac6f64ed306c67a563
mayongbin <mayongbin@meituan.com>
2023-12-26 10:29:08 +0800

    feat: remove unused code.

--- instagdm/src/test/java/top/archer/instagdm/requests/InsRequestsTest.java
+++ instagdm/src/test/java/top/archer/instagdm/requests/InsRequestsTest.java
@@ -1,8 +1,5 @@
 package top.archer.instagdm.requests;
 
-import com.github.instagram4j.instagram4j.IGClient;
-import org.apache.commons.lang3.tuple.Pair;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,10 +19,6 @@ public class InsRequestsTest {
 
     @Test
     public void testSearchUser() {
-        Optional<IGClient> opClient = loginService.login("archervanderwaal", "ArcherVanderWaal0221@");
-        Assertions.assertTrue(opClient.isPresent());
-        Pair<byte[], byte[]> results = serializeService.serialize(opClient.get());
-        IGClient cli = serializeService.deserialize(results);
-        Assertions.assertEquals("archervanderwaal", cli.getSelfProfile().getUsername());
+
     }
 }
